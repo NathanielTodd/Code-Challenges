ab2388a985f0e38792c3f291ed49a1a628f92266
Nathaniel Todd <nate4842@gmail.com>
2018-10-28 13:14:57 -0400

    Create AddTwoNumbers.cpp

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        if (l1 == NULL) return l2;
+        if (l2 == NULL) return l1;
+        
+        ListNode* head = new ListNode(0);
+        ListNode* curr = new ListNode(0);      
+        int val1, val2;
+        
+        //Set current next to solution head
+        curr->next = head;
+        
+        //iterate while both l1 and l2 have some value
+        while(l1 || l2){
+            
+            //if none exists there was no carried one
+            //but enetered loop so more digits exist
+            //need to initialize next and iterate
+            if(curr->next==NULL){
+                curr->next = new ListNode(0); 
+            }
+            curr = curr->next;
+            
+            //set vals and iterate depending on state
+            if(l1==NULL){
+                //set addition vals
+                val1 = 0;
+                val2 = l2->val;
+                
+                //iterate to next node
+                l1 = NULL;
+                l2 = l2->next;
+            }
+            else if(l2==NULL){
+                //set addition vals
+                val2 = 0;
+                val1 = l1->val;
+                
+                //iterate to next node
+                l2 = NULL;
+                l1 = l1->next;
+            }
+            else{
+                //set addition vals
+                val1 = l1->val;
+                val2 = l2->val;
+                
+                //iterate to next node
+                l1 = l1->next;
+                l2 = l2->next;
+            }
+            
+            //test if addition is greater than 10
+            //pass 1 to next node if it is
+            if((curr->val + val1 + val2)>9){
+                curr->next = new ListNode(1);
+            }
+            
+            //set current digit to remainder
+            curr->val = (curr->val+val1+val2)%10;
+        }
+        
+        return head;
+        
+    }
+};
