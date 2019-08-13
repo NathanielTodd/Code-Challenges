5a93505bad960d8262f5abb3b3372c90161e033d
Nathaniel Todd <nate4842@gmail.com>
2019-08-13 18:38:32 -0400

    Create ReverseInteger.cpp

---
+++ ReverseInteger.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int remainder = 0;
+        int output = 0;
+        bool negFlag = false;
+        
+        if(x==0) return 0;
+        else if(x<0){
+            negFlag = true;
+            x = -1*x;
+        }
+        
+        while(x>0){
+            remainder = x%10;
+            output = output*10 + remainder;
+            x = x/10;
+        }
+        
+        if(negFlag) output = -1*;
+        
+        return output;
+    }
+};
