8788bdd4a286adc9201782383d6bc02a2ef5da86
Nathaniel Todd <nate4842@gmail.com>
2019-08-14 10:42:31 -0400

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,3 +1,34 @@
+/*
+Author: Nathaniel Todd
+Date Updated: 08/14/29
+
+Problem Description:
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+Follow up:
+
+Coud you solve it without converting the integer to a string?
+
+Explanation:
+In order to determine if an int is a palindrome, I isolate the digits and build a reversed
+number. In reality, I only have to do this for the first half of the digits and compare it 
+against the latter half of the digits.
+*/
+
 class Solution {
 public:
     bool isPalindrome(int x) {
