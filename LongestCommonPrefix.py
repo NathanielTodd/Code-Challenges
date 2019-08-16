17f40b1ccab44c91ca57e357dec46684e8d66ffe
Nathaniel Todd <nate4842@gmail.com>
2019-08-16 17:13:38 -0400

    Update LongestCommonPrefix.py

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -20,20 +20,25 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
             
+        #if list is empty return an empty string
         if(len(strs)==0): return "" 
-
+        
+        # only need to iterate up to the shortest string length
+        # if any of the strings are empty reutrn an empty string
         l = sys.maxsize
         for s in strs:
             if(len(s)==0): return ""
             elif(len(s)<l): l = len(s)
         
         i = 0
+        # iterate through and check each character
         for i in range(l):
+            
+            # return the substring prefix up to that point if a mismatch is found
             c = strs[0][i]
             for s in strs:
-                if(s[i]==c):
-                    continue
-                else:
+                if(s[i]!=c):
                     return strs[0][0:i]
-
+                
+        #otherwise return the shortest string as the prefix
         return strs[0][0:i+1]
